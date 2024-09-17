5e36e048fe699ababef13ba486d9dbfa9bdf5fa8
Pontus Hedman <ponhe215@student.liu.se>
2024-09-18 00:33:28 +0200

    Actions done

--- labs/lab1/reactive/src/template/Actions.java
+++ labs/lab1/reactive/src/template/Actions.java
@@ -1,5 +1,33 @@
 package template;
 
-public class Actions {
+import logist.topology.Topology.City;
 
+public class Actions {
+    private City startCity;
+    private City endCity;
+    private boolean pickUp;
+
+    public City getStartCity() {
+       return startCity;
+    }
+
+    public City getEndCity() {
+        return endCity;
+    }
+
+    public boolean isPickUp() {
+        return pickUp;
+    }
+
+    public void setStartCity(City startCity) {
+        this.startCity = startCity;
+    }
+
+    public void setEndCity(City endCity) {
+        this.endCity = endCity;
+    }
+
+    public void setPickUp(boolean pickUp) {
+        this.pickUp = pickUp;
+    }
 }
