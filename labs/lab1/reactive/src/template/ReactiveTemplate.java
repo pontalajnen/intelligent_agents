856bed818f5dd19e9a79830869fc77b62e743964
Erik Bessö <erik.besso@pihr.com>
2024-09-19 14:38:29 +0200

    Progress on lab 1

--- labs/lab1/reactive/src/template/ReactiveTemplate.java
+++ labs/lab1/reactive/src/template/ReactiveTemplate.java
@@ -32,6 +32,33 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		this.pPickup = discount;
 		this.numActions = 0;
 		this.myAgent = agent;
+
+		double[] V = new double[topology.cities().size()];
+		double[][][] Q = new double [topology.cities().size()][topology.cities().size()][2];
+		Vehicle v = agent.vehicles().get(0);
+		double diff = 1e10;
+		double epsilon = 1e-5;
+
+		while (diff > epsilon){
+			for(City city1 : topology.cities()){
+				for(City city2 : topology.cities()){
+					if (city1.hasNeighbor(city2)) {
+						var oldQ = Q[city1.id][city2.id][0];
+						Q[city1.id][city2.id][0] = -city1.distanceTo(city2) * v.costPerKm() +
+								discount * (1 - td.probability(city1, city2)) * V[city2.id];
+						V[city1.id] = Math.max(V[city1.id], Q[city1.id][city2.id][0]);
+						diff = Math.max(diff, Math.abs(Q[city1.id][city2.id][0] - oldQ));
+
+					}
+
+				}
+
+			}
+		}
+
+
+
+
 	}
 
 	@Override
@@ -44,12 +71,12 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		} else {
 			action = new Pickup(availableTask);
 		}
-		
+
 		if (numActions >= 1) {
 			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
 		}
 		numActions++;
-		
+
 		return action;
 	}
 }
