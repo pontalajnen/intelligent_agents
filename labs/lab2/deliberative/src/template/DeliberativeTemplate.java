cd42b60d870fd1749b0168a42a46071135f359f0
Erik Bessö <erik.besso@pihr.com>
2024-10-02 16:18:03 +0200

    exercise 2 progress

--- labs/lab2/deliberative/src/template/DeliberativeTemplate.java
+++ labs/lab2/deliberative/src/template/DeliberativeTemplate.java
@@ -1,15 +1,19 @@
 package template;
 
 /* import table */
+import logist.plan.Action;
 import logist.simulation.Vehicle;
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
 import logist.plan.Plan;
+import logist.simulation.VehicleImpl;
 import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
+
+import java.util.*;
 
 /**
  * An optimal planner for one vehicle.
@@ -18,47 +22,132 @@ import logist.topology.Topology.City;
 public class DeliberativeTemplate implements DeliberativeBehavior {
 
 	enum Algorithm { BFS, ASTAR }
-	
+
 	/* Environment */
 	Topology topology;
 	TaskDistribution td;
-	
+
 	/* the properties of the agent */
 	Agent agent;
 	int capacity;
 
 	/* the planning class */
 	Algorithm algorithm;
-	
+
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 		this.topology = topology;
 		this.td = td;
 		this.agent = agent;
-		
+
 		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
 		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
-		
+
 		// Throws IllegalArgumentException if algorithm is unknown
 		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
-		
+
 		// ...
 	}
-	
+
 	@Override
 	public Plan plan(Vehicle vehicle, TaskSet tasks) {
 		Plan plan;
 
-		// Compute the plan with the selected algorithm.
 		switch (algorithm) {
 		case ASTAR:
 			// ...
 			plan = naivePlan(vehicle, tasks);
 			break;
 		case BFS:
-			// ...
-			plan = naivePlan(vehicle, tasks);
+			Queue<State> queue = new LinkedList<>();
+
+			var initialState = new State(
+					null,
+					vehicle.getCurrentCity(),
+					0,
+					TaskSet.noneOf(tasks),
+					TaskSet.copyOf(tasks),
+					TaskSet.noneOf(tasks),
+					new ArrayList<>()
+			);
+
+			queue.add(initialState);
+
+			State terminalState = null;
+
+			while(!queue.isEmpty()){
+
+				var currentState = queue.poll();
+				if (terminalState != null && currentState.getTotalCost() > terminalState.getTotalCost()){
+					continue;
+				}
+				if(currentState.getRemainingTasks().isEmpty()){
+					if(terminalState == null || terminalState.getTotalCost() > currentState.getTotalCost()){
+						terminalState = currentState;
+					}
+					continue;
+				}
+				TaskSet deliverableTasks = TaskSet.copyOf(currentState.getCarryingTasks());
+
+				List<Action> currentPlan = currentState.getActions();
+
+				for(var task:currentState.getCarryingTasks()){
+					if (task.deliveryCity != currentState.getLocation()){
+						deliverableTasks.remove(task);
+						currentPlan.add(new Action.Delivery(task));
+					}
+				}
+				currentState.setCarryingTasks(TaskSet.intersectComplement(currentState.getCarryingTasks(), deliverableTasks));
+				currentState.setDeliveredTasks(TaskSet.union(currentState.getDeliveredTasks(), deliverableTasks));
+
+				TaskSet cityTasks = TaskSet.noneOf(currentState.getRemainingTasks());
+
+				for(var task:currentState.getRemainingTasks()){
+					if (task.pickupCity == currentState.getLocation()){
+						cityTasks.add(task);
+					}
+				}
+				var permutationCounter = Math.pow(2, cityTasks.size());
+
+				for(var i = 0; i < permutationCounter; i++){
+					var newPlan = new ArrayList<Action>(currentPlan);
+					var permutation = Integer.toBinaryString(i);
+					while (permutation.length() < cityTasks.size()){
+						permutation = "0" + permutation;
+					}
+
+					TaskSet potentialPickup = TaskSet.noneOf(cityTasks);
+					int j = 0;
+					for(var task:cityTasks){
+						if (permutation.charAt(j) == '1'){
+							newPlan.add(new Action.Pickup(task));
+							potentialPickup.add(task);
+						}
+						j++;
+					}
+					if (potentialPickup.weightSum() < vehicle.capacity() - currentState.getCarryingTasks().weightSum()){
+						for (var neighbor: currentState.getLocation().neighbors()){
+							var newerPlan = new ArrayList<>(newPlan);
+							newerPlan.add(new Action.Move(neighbor));
+							queue.add(new State(
+									currentState,
+									neighbor,
+									currentState.getTotalCost() + currentState.getLocation().distanceUnitsTo(neighbor) * vehicle.costPerKm(),
+									TaskSet.union(currentState.getCarryingTasks(), potentialPickup),
+									TaskSet.intersectComplement(currentState.getRemainingTasks(), potentialPickup),
+									currentState.getDeliveredTasks(),
+									newerPlan));
+						}
+					}
+				}
+			}
+			//Construct plan
+			plan = new Plan(vehicle.getCurrentCity());
+			for(var action: terminalState.getActions()) {
+				plan.append(action);
+			}
+			System.out.println("Plan Competed!");
 			break;
 		default:
 			throw new AssertionError("Should not happen.");
---
+++ labs/lab2/deliberative/src/template/State.java
@@ -0,0 +1,85 @@
+package template;
+
+import logist.plan.Action;
+import logist.plan.Plan;
+import logist.task.TaskSet;
+import logist.topology.Topology;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class State {
+    private State parentState;
+    private Topology.City location;
+    private double totalCost;
+    private TaskSet carryingTasks;
+    private TaskSet remainingTasks;
+    private TaskSet deliveredTasks;
+    private List<Action> actions;
+
+    public State(State parentState, Topology.City location, double totalCost, TaskSet carryingTasks, TaskSet remainingTasks, TaskSet deliveredTasks, List<Action> actions) {
+        this.parentState = parentState;
+        this.location = location;
+        this.totalCost = totalCost;
+        this.carryingTasks = carryingTasks;
+        this.remainingTasks = remainingTasks;
+        this.deliveredTasks = deliveredTasks;
+        this.actions = actions;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    public State getParentState() {
+        return parentState;
+    }
+
+    public TaskSet getDeliveredTasks() {
+        return deliveredTasks;
+    }
+
+    public void setDeliveredTasks(TaskSet deliveredTasks) {
+        this.deliveredTasks = deliveredTasks;
+    }
+
+    public void setParentState(State parentState) {
+        this.parentState = parentState;
+    }
+
+    public Topology.City getLocation() {
+        return location;
+    }
+
+    public void setLocation(Topology.City location) {
+        this.location = location;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public TaskSet getCarryingTasks() {
+        return carryingTasks;
+    }
+
+    public void setCarryingTasks(TaskSet carryingTasks) {
+        this.carryingTasks = carryingTasks;
+    }
+
+    public TaskSet getRemainingTasks() {
+        return remainingTasks;
+    }
+
+    public void setRemainingTasks(TaskSet remainingTasks) {
+        this.remainingTasks = remainingTasks;
+    }
+}
