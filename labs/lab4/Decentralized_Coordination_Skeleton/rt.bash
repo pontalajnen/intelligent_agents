7a4af3f826be98d1d945f67e192fd6687ddd5633
Pontus Hedman <ponhe215@student.liu.se>
2024-11-22 17:15:20 +0100

    Updated bash

--- labs/lab4/Decentralized_Coordination_Skeleton/rt.bash
+++ labs/lab4/Decentralized_Coordination_Skeleton/rt.bash
@@ -8,5 +8,11 @@ cd "tournament"
 rm -rf *
 cd ..
 
+mv "out/artifacts/Decentralized_Coordination_Skeleton_jar/Decentralized_Coordination_Skeleton.jar" "agents/"
+cd "agents"
+rm "our_agent.jar"
+mv "Decentralized_Coordination_Skeleton.jar" "our_agent.jar"
+cd ..
+
 # Create the tournament
 java -jar ./logist/logist.jar -new $1 ./agents && java -jar ./logist/logist.jar -run $1 ./config/auction.xml && java -jar ./logist/logist.jar -score $1
